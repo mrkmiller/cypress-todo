5d59aa2c49dfb6dad402df51f519a1bbca519c92
Mark Miller <mrkmiller@ucdavis.edu>
2022-07-11 22:15:39 -0700

    github actions

--- cypress/e2e/4-fixture-for-tasks.cy.js
+++ cypress/e2e/4-fixture-for-tasks.cy.js
@@ -1,13 +1,10 @@
 /// <reference types="Cypress" />
 
 describe('4. Fixture for default tasks', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
   // https://docs.cypress.io/api/commands/intercept
   it('Intercept an API call and fake the response', () => {
     cy.intercept('**/todos', { fixture: 'todos.json' })
+    cy.visit('/')
 
     // Remove the task.
     cy.contains('Fixture Task')
