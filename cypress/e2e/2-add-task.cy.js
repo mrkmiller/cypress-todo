2cbc79aaea0cafab420534bba265aacab650c3c4
Mark Miller <mrkmiller@ucdavis.edu>
2022-08-08 17:24:59 -0700

    comment out

--- cypress/e2e/2-add-task.cy.js
+++ cypress/e2e/2-add-task.cy.js
@@ -10,25 +10,25 @@ describe('2. Add Items', () => {
      * 1. Add a new task and ensure it exists in the list of todos.
      */
 
-    cy.get('.new-todo')
-      .type('Teach Cypress{enter}') // Use {enter} to press the "enter" key.
-    cy.contains('.todo-list', 'Teach Cypress')
+    // cy.get('.new-todo')
+    //   .type('Teach Cypress{enter}') // Use {enter} to press the "enter" key.
+    // cy.contains('.todo-list', 'Teach Cypress')
 
     /**
      * 2. Edit the message by double-clicking the task's text.
      */
 
-    cy.contains('.todo-list li', 'Teach Cypress')
-      .dblclick()
-      .type(' Altered{enter}')
+    // cy.contains('.todo-list li', 'Teach Cypress')
+    //   .dblclick()
+    //   .type(' Altered{enter}')
 
     /**
      * 3. Complete the "Teach Cypress" task by clicking its toggle button.
      */
 
-    cy.contains('.todo-list li', 'Teach Cypress Altered')
-      .find('.toggle')
-      .click()
+    // cy.contains('.todo-list li', 'Teach Cypress Altered')
+    //   .find('.toggle')
+    //   .click()
   })
 
 })
