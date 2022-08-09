bd1dffaa59e4e70611b3cfe0d6ea5b23a907884b
Mark Miller <mrkmiller@ucdavis.edu>
2022-08-08 17:16:57 -0700

    better comments for test 4

--- cypress/e2e/4-fixture-for-tasks.cy.js
+++ cypress/e2e/4-fixture-for-tasks.cy.js
@@ -2,6 +2,10 @@
 
 describe('4. Fixture for default tasks', () => {
   beforeEach(() => {
+    /**
+     * 1. Intercept the Ajax Fetch and replace with fixture data.
+     */
+
     cy.intercept('**/todos', { fixture: 'todos.json' })
     cy.visit('/')
   })
@@ -9,7 +13,7 @@ describe('4. Fixture for default tasks', () => {
   // https://docs.cypress.io/api/commands/intercept
   it('Intercept an API call and fake the response', () => {
     /**
-     * 1. Tasks should show fixture data.
+     * 2. Tasks should show fixture data instead of remote API data.
      */
 
     cy.contains('Fixture 1')
