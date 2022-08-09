8c73ab22c120cbc1c9e133a3ca67dcf572f481c9
Mark Miller <mrkmiller@ucdavis.edu>
2022-08-08 17:23:56 -0700

    comment out

--- cypress/e2e/3-complete-task.cy.js
+++ cypress/e2e/3-complete-task.cy.js
@@ -15,35 +15,35 @@ describe('3. Complete Tasks', () => {
      * 1. Assert that the new "Teach Cypress" task is complete.
      */
 
-    cy.contains('.todo-list li', 'Teach Cypress')
-      .should('have.class', 'completed')
+    // cy.contains('.todo-list li', 'Teach Cypress')
+    //   .should('have.class', 'completed')
 
     /**
      * 2. Ensure the "Active" status filter does not show completed tasks.
      */
 
-    cy.get('[data-active]').click()
-    cy.get('.todo-list')
-      .should('contain', 'Have Fun')
-      .should('not.contain', 'Teach Cypress')
+    // cy.get('[data-active]').click()
+    // cy.get('.todo-list')
+    //   .should('contain', 'Have Fun')
+    //   .should('not.contain', 'Teach Cypress')
 
     /**
      * 3. Ensure the "Completed" status filter does not show active tasks.
      */
 
-    cy.get('[data-completed]').click()
-    cy.get('.todo-list')
-      .should('contain', 'Teach Cypress')
-      .should('not.contain', 'Have Fun')
+    // cy.get('[data-completed]').click()
+    // cy.get('.todo-list')
+    //   .should('contain', 'Teach Cypress')
+    //   .should('not.contain', 'Have Fun')
 
     /**
      * 4. Clear all completed tasks.
      */
 
-    cy.get('[data-all]').click()
-    cy.get('.clear-completed')
-      .click()
-    cy.get('.todo-list').should('not.contain', 'Teach Cypress')
+    // cy.get('[data-all]').click()
+    // cy.get('.clear-completed')
+    //   .click()
+    // cy.get('.todo-list').should('not.contain', 'Teach Cypress')
   })
 
 })
