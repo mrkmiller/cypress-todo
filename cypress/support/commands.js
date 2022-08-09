c307f74116d8658d51e2dc6a155b1c9afd3f7eb2
Mark Miller <mrkmiller@ucdavis.edu>
2022-08-08 17:29:24 -0700

    remove custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,39 +23,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-/**
- * Create a new Issue Task.
- *
- * @param {string} name - The title text of the new issue.
- * @param {boolean} complete - The issue status for if it is complete or not.
- */
-Cypress.Commands.add('addTask', (name, complete = false) => {
-  /**
-   * 1. Create a custom log message.
-   */
-
-  // cy.log(`Add task: ${name}`)
-
-  /**
-   * 2. Create the task and ensure it exists.
-   */
-
-  // cy.get('.new-todo', {log: false})
-  //   .type(`${name}{enter}`, {log: false})
-  // cy.contains('.todo-list li', name, {log: false})
-  //   .should('exist', {log: false})
-
-  /**
-   * 3. Toggle the "Complete" status button if this task is already complete and
-   *    ensure the that it is now marked as complete.
-   */
-
-  // if (complete) {
-  //   cy.contains('.todo-list li', name, {log: false})
-  //     .find('.toggle', {log: false})
-  //     .click({log: false})
-  //   cy.contains('.todo-list li', name, {log: false})
-  //     .should('have.class', 'completed')
-  // }
-})
